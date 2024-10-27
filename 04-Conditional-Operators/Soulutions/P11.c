fe46ccbf459e2e01740650953cd80156716f2b12
Vasil Zahariev <vasilzahariev1707@abv.bg>
2024-10-27 12:13:53 +0200

    fix: using epsilon where needed

--- 04-Conditional-Operators/Soulutions/P11.c
+++ 04-Conditional-Operators/Soulutions/P11.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
+    const double k_epsilon = 0.000001;
+    
     double firstSide = 0;
     double secondSide = 0;
     double thirdSide = 0;
@@ -8,13 +11,13 @@ int main() {
     scanf("%lf %lf %lf", &firstSide, &secondSide, &thirdSide);
 
     if (
-        (firstSide <= 0 || secondSide <= 0 || thirdSide <= 0) ||
-        (firstSide + secondSide <= thirdSide || firstSide + thirdSide <= secondSide || secondSide + thirdSide <= firstSide)
+        (firstSide < k_epsilon || secondSide < k_epsilon || thirdSide < k_epsilon) ||
+        (firstSide + secondSide < thirdSide + k_epsilon || firstSide + thirdSide < secondSide + k_epsilon || secondSide + thirdSide < firstSide + k_epsilon)
     ) {
         printf("not a triangle\n");
     } else if (firstSide == secondSide && secondSide == thirdSide) {
         printf("equilateral\n");
-    } else if (firstSide == secondSide || firstSide == thirdSide || secondSide == thirdSide) {
+    } else if (fabs(firstSide - secondSide) < k_epsilon || fabs(firstSide - thirdSide) < k_epsilon || fabs(secondSide - thirdSide) < k_epsilon) {
         printf("isosceles\n");
     } else {
         printf("scalene\n");
--- 04-Conditional-Operators/Soulutions/P12.c
+++ 04-Conditional-Operators/Soulutions/P12.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+    const double k_epsilon = 0.000001;
     const double k_rectStartX = 0.0;
     const double k_rectStartY = 0.0;
     const double k_rectEndX = 5.0;
@@ -13,7 +14,7 @@ int main() {
 
     if (x < k_rectStartX || x > k_rectEndX || y < k_rectStartY || y > k_rectEndY) {
         printf("outside the area\n");
-    } else if ((x == k_rectStartX || x == k_rectEndX) && (y == k_rectStartY || y == k_rectEndY)) {
+    } else if ((fabs(x - k_rectStartX) < k_epsilon || fabs(x - k_rectEndX) < k_epsilon) && (fabs(y - k_rectStartY) < k_epsilon || fabs(y - k_rectEndY) < k_epsilon)) {
         printf("on the border\n");
     } else {
         printf("inside the area\n");
